29459ea961179a5252fa225089ee3918d2ccda73
Tanvir Sajal <t.haque.ju@gmail.com>
2020-11-14 11:16:37 +0900

    horoku

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ const search=document.querySelector('input')
 weatherForm.addEventListener('submit',(e)=>{
 	e.preventDefault()
 	const location= search.value
-	fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+	fetch('/weather?address='+location).then((response)=>{
 	response.json().then((data)=>{
 		if (data.error) {
 			console.log('vul ase')
